4a0f1c4a668746a4ddfd50d0c652abe1834a4e8c
DragonBillow <DragonBillow@outlook.com>
2024-03-23 13:28:44 +0800

    refactor(code): remove some useless code
    Signed-off-by: DragonBillow <DragonBillow@outlook.com>

--- src/code/gallery_extension.rs
+++ src/code/gallery_extension.rs
@@ -15,35 +15,15 @@ pub struct IRawGalleryQueryResult {
 }
 
 #[api]
-pub struct IRawGalleryExtensionStatistics {
-    pub statistic_name: String,
-    pub value: f64,
-}
-
-#[api]
 pub struct IRawGalleryExtensionPublisher {
-    pub display_name: String,
-    pub publisher_id: String,
     pub publisher_name: String,
-    pub domain: Option<String>,
-    pub is_domain_verified: bool,
 }
 
 #[api]
 pub struct IRawGalleryExtension {
-    pub extension_id: String,
     pub extension_name: String,
-    pub display_name: String,
-    pub short_description: Option<String>,
     pub publisher: IRawGalleryExtensionPublisher,
     pub versions: Vec<IRawGalleryExtensionVersion>,
-    pub statistics: Vec<IRawGalleryExtensionStatistics>,
-    pub tags: Option<String>,
-    pub release_date: String,
-    pub published_date: String,
-    pub last_updated: String,
-    pub categories: Option<Vec<String>>,
-    pub flags: String,
 }
 
 impl Display for IRawGalleryExtension {
