26654ba1f058f31f0cad21564be34ba2b045d9b2
Jacob Birkett <jacob@birkett.dev>
2024-08-26 20:27:03 -0700

    flake: devShell: add rust nightly for tools

--- flake.nix
+++ flake.nix
@@ -26,12 +26,24 @@
       rustToolchain = lib.importTOML ./rust-toolchain.toml;
       rustVersion = rustToolchain.toolchain.channel;
     in {
-      devShells = lib.mapAttrs (system: pkgs: {
-        default = pkgs.mkShell {
-          strictDeps = true;
-          buildInputs = [ pkgs.rust-bin.stable.${rustVersion}.default ];
-        };
-      }) pkgsFor;
+      devShells = lib.mapAttrs (system: pkgs:
+        let
+          rust-stable = pkgs.rust-bin.stable.${rustVersion}.minimal.override {
+            extensions = [ "rust-src" "rust-docs" "clippy" ];
+          };
+        in {
+          default = pkgs.mkShell {
+            strictDeps = true;
+            packages = [
+              (lib.hiPrio rust-stable)
+              # Use rustfmt, and other tools that require nightly features.
+              (pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+                toolchain.minimal.override {
+                  extensions = [ "rustfmt" "rust-analyzer" ];
+                }))
+            ];
+          };
+        }) pkgsFor;
 
       overlays = {
         default = lib.composeManyExtensions [ self.overlays.${packageName} ];
