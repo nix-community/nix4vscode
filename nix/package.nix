7acb20206a4c4abb86fe2a420909f06109f0997d
Jacob Birkett <jacob@birkett.dev>
2024-08-26 21:06:31 -0700

    nix: package: inherit meta from cargo manifest

--- nix/package.nix
+++ nix/package.nix
@@ -6,4 +6,10 @@ in rustPlatform.buildRustPackage {
   cargoLock.lockFile = "${sourceRoot}/Cargo.lock";
   src = lib.cleanSource sourceRoot;
   strictDeps = true;
+  meta = {
+    inherit (manifest.package) description homepage;
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.unix;
+    mainProgram = manifest.package.name;
+  };
 }
