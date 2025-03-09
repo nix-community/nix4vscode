acdd7151333b37fbf8fba4b239be3ca284e4efe8
loongtao.zhang <loongtao.zhang@outlook.com>
2025-03-09 21:25:23 +0800

    feat: add exporter
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

---
+++ crates/exporter/src/models.rs
@@ -0,0 +1,14 @@
+use diesel::prelude::*;
+
+#[derive(Queryable, Selectable, Insertable)]
+#[diesel(table_name = crate::schema::marketplace)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Marketplace {
+    pub name: String,
+    pub publisher: String,
+    pub version: String,
+    pub engine: String,
+    pub platform: String,
+    pub assert_url: String,
+    pub hash: Option<String>,
+}
