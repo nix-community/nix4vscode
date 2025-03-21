720983ee973c29fbd97c18db1418617945648514
loongtao.zhang <loongtao.zhang@outlook.com>
2025-03-21 16:19:14 +0800

    feat(exporter): skip if platform is universal
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- crates/exporter/src/models.rs
+++ crates/exporter/src/models.rs
@@ -19,9 +19,14 @@ pub struct Marketplace {
     pub publisher: String,
     pub version: String,
     pub engine: String,
+    #[serde(skip_serializing_if = "is_universal")]
     pub platform: String,
     pub assert_url: String,
     #[serde(skip)]
     pub is_prerelease: bool,
     pub hash: Option<String>,
+}
+
+fn is_universal(name: &str) -> bool {
+    name == "universal"
 }
