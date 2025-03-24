5218238b9978369335b829cced2bf13c9f7fe31d
loongtao.zhang <loongtao.zhang@outlook.com>
2025-03-24 11:37:53 +0800

    fix(overlay): fix cpptools
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- nix/decorators/ms-vscode.cpptools.nix
+++ nix/decorators/ms-vscode.cpptools.nix
@@ -8,38 +8,9 @@
   clang-tools ? pkgs.clang-tools,
 }:
 
-let
-  isx86Linux = system == "x86_64-linux";
-  isLinux = system == "x86_64-linux" || system == "aarch64-linux";
-  gdbUseFixedOverlay = gdbUseFixed && isLinux;
-in
 {
-  nativeBuildInputs =
-    with pkgs;
-    [
-      makeWrapper
-    ]
-    ++ lib.lists.optional isLinux [
-      autoPatchelfHook
-    ];
-
-  buildInputs =
-    with pkgs;
-    [
-      jq
-      libkrb5
-      zlib
-      (lib.getLib stdenv.cc.cc)
-    ]
-    ++ lib.lists.optional isLinux [
-      lttng-ust
-    ];
-
-  dontAutoPatchelf = isx86Linux;
 
   postPatch = ''
-    mv ./package.json ./package_orig.json
-
     # 1. Add activation events so that the extension is functional. This listing is empty when unpacking the extension but is filled at runtime.
     # 2. Patch `package.json` so that nix's *gdb* is used as default value for `miDebuggerPath`.
     # Prevent download/install of extensions
@@ -60,28 +31,5 @@ in
     }
 
     make_executable bin/cpptools bin/cpptools-srv debugAdapters/bin/OpenDebugAD7 bin/libc.so bin/cpptools-wordexp
-    #
-    patch_elf() {
-      for file in "''$@"; do
-        if [[ -e "''$file" ]]; then
-          patchelf --replace-needed "''$file"
-        fi
-      done
-    }
-    patch_elf liblttng-ust.so.0 liblttng-ust.so.1 ./debugAdapters/bin/libcoreclrtraceptprovider.so
   '';
-
-  # On aarch64 the binaries are statically linked
-  # but on x86 they are not.
-  postFixup =
-    lib.optionalString isx86Linux ''
-      autoPatchelf $out/share/vscode/extensions/ms-vscode.cpptools/debugAdapters
-      # cpptools* are distributed by the extension and need to be run through the distributed musl interpretter
-      patchelf --set-interpreter $out/share/vscode/extensions/ms-vscode.cpptools/bin/libc.so $out/share/vscode/extensions/ms-vscode.cpptools/bin/cpptools
-      patchelf --set-interpreter $out/share/vscode/extensions/ms-vscode.cpptools/bin/libc.so $out/share/vscode/extensions/ms-vscode.cpptools/bin/cpptools-srv
-      patchelf --set-interpreter $out/share/vscode/extensions/ms-vscode.cpptools/bin/libc.so $out/share/vscode/extensions/ms-vscode.cpptools/bin/cpptools-wordexp
-    ''
-    + lib.optionalString gdbUseFixedOverlay ''
-      wrapProgram $out/share/vscode/extensions/ms-vscode.cpptools/debugAdapters/bin/OpenDebugAD7 --prefix PATH : ${lib.makeBinPath [ gdb ]}
-    '';
 }
