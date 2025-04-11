e8f9ced37717ff834dcff675d6fef90c4ad734f3
loongtao.zhang <loongtao.zhang@outlook.com>
2025-04-11 10:17:48 +0800

    fix(plugins): try fix sumneko.lua
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

---
+++ nix/decorators/sumneko.lua.nix
@@ -0,0 +1,22 @@
+{
+  pkgs ? import <nixpkgs> { },
+  lib ? pkgs.lib,
+  system ? builtins.currentSystem,
+}:
+
+{
+
+  postPatch = ''
+    # Patching binaries
+    make_executable() {
+      for file in "''$@"; do
+        if [[ -e "''$file" ]]; then
+          chmod +x "''$file"
+        fi
+      done
+    }
+
+    make_executable server/bin/lua-language-server
+    sed -i "s/await fs.promises.chmod(command, \"777\");//g" client/out/src/languageserver.js
+  '';
+}
