1bc12392242c34b814338346a144877accc37e7f
loongtao.zhang <loongtao.zhang@outlook.com>
2025-06-27 10:25:15 +0800

    fix(exporter): fix name parse
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- crates/exporter/src/fetch_info.rs
+++ crates/exporter/src/fetch_info.rs
@@ -29,6 +29,7 @@ pub async fn fetch_marketplace(
     ));
     let codelldb = fetch_codelldb().await.unwrap_or_default();
     debug!("codelldb: len = {}", codelldb.len());
+
     let mut extension_count = 0usize;
     let mut all_count = 0usize;
     while let Some(item) = iter.next().await {
@@ -137,18 +138,425 @@ fn drain_codelldb(codelldb: &HashMap<TagName, Release>, item: Marketplace) -> Ve
     x
 }
 
+fn is_darwin(value: &str) -> bool {
+    value.contains("darwin")
+}
+
+fn is_linux(value: &str) -> bool {
+    value.contains("linux")
+}
+
+fn is_aarch64(value: &str) -> bool {
+    value.contains("aarch64") || value.contains("arm")
+}
+
+fn is_x86_64(value: &str) -> bool {
+    value.contains("x86_64") || value.contains("x64")
+}
+
 fn is_darwin_aarch64(value: &str) -> bool {
-    value.contains("darwin") && value.contains("aarch64")
+    is_darwin(value) && is_aarch64(value)
 }
 
 fn is_darwin_x86_64(value: &str) -> bool {
-    value.contains("darwin") && value.contains("x86_64")
+    is_darwin(value) && is_x86_64(value)
 }
 
 fn is_linux_x86_64(value: &str) -> bool {
-    value.contains("linux") && value.contains("x86_64")
+    is_linux(value) && is_x86_64(value)
 }
 
 fn is_linux_aarch64(value: &str) -> bool {
-    value.contains("linux") && value.contains("arm")
+    is_linux(value) && is_aarch64(value)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_codelldb() {
+        fn nonp(_: &str) -> bool {
+            true
+        }
+
+        #[allow(clippy::type_complexity)]
+        let cases: [(&str, &str, fn(&str) -> bool); 203] = [
+            ("v1.9.0", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.9.0", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.9.0", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.9.0", "codelldb-bootstrap.vsix", nonp),
+            ("v1.9.0", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.9.0", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.9.0", "codelldb-x86_64-windows.vsix", nonp),
+            ("v1.11.5", "codelldb-bootstrap.vsix", nonp),
+            ("v1.11.5", "codelldb-darwin-arm64.vsix", is_darwin_aarch64),
+            ("v1.11.5", "codelldb-darwin-x64.vsix", is_darwin_x86_64),
+            ("v1.11.5", "codelldb-linux-arm64.vsix", is_linux_aarch64),
+            ("v1.11.5", "codelldb-linux-armhf.vsix", is_linux_aarch64),
+            ("v1.11.5", "codelldb-linux-x64.vsix", is_linux_x86_64),
+            ("v1.11.5", "codelldb-win32-x64.vsix", nonp),
+            ("v1.7.2", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.7.2", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.7.2", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.7.2", "codelldb-bootstrap.vsix", nonp),
+            ("v1.7.2", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.7.2", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.7.2", "codelldb-x86_64-windows.vsix", nonp),
+            ("v1.8.1", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.8.1", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.8.1", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.8.1", "codelldb-bootstrap.vsix", nonp),
+            ("v1.8.1", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.8.1", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.8.1", "codelldb-x86_64-windows.vsix", nonp),
+            ("v1.11.3", "codelldb-bootstrap.vsix", nonp),
+            ("v1.11.3", "codelldb-darwin-arm64.vsix", is_darwin_aarch64),
+            ("v1.11.3", "codelldb-darwin-x64.vsix", is_darwin_x86_64),
+            ("v1.11.3", "codelldb-linux-arm64.vsix", is_linux_aarch64),
+            ("v1.11.3", "codelldb-linux-armhf.vsix", is_linux_aarch64),
+            ("v1.11.3", "codelldb-linux-x64.vsix", is_linux_x86_64),
+            ("v1.11.3", "codelldb-win32-x64.vsix", nonp),
+            ("v1.11.0", "codelldb-bootstrap.vsix", nonp),
+            ("v1.11.0", "codelldb-darwin-arm64.vsix", is_darwin_aarch64),
+            ("v1.11.0", "codelldb-darwin-x64.vsix", is_darwin_x86_64),
+            ("v1.11.0", "codelldb-linux-arm64.vsix", is_linux_aarch64),
+            ("v1.11.0", "codelldb-linux-armhf.vsix", is_linux_aarch64),
+            ("v1.11.0", "codelldb-linux-x64.vsix", is_linux_x86_64),
+            ("v1.11.0", "codelldb-win32-x64.vsix", nonp),
+            ("v1.7.3", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.7.3", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.7.3", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.7.3", "codelldb-bootstrap.vsix", nonp),
+            ("v1.7.3", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.7.3", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.7.3", "codelldb-x86_64-windows.vsix", nonp),
+            ("v1.10.0", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.10.0", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.10.0", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.10.0", "codelldb-bootstrap.vsix", nonp),
+            ("v1.10.0", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.10.0", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.10.0", "codelldb-x86_64-windows.vsix", nonp),
+            (
+                "v1.6.8-dev.2110042330",
+                "codelldb-aarch64-darwin.vsix",
+                is_darwin_aarch64,
+            ),
+            (
+                "v1.6.8-dev.2110042330",
+                "codelldb-aarch64-linux.vsix",
+                is_linux_aarch64,
+            ),
+            (
+                "v1.6.8-dev.2110042330",
+                "codelldb-arm-linux.vsix",
+                is_linux_aarch64,
+            ),
+            ("v1.6.8-dev.2110042330", "codelldb-bootstrap.vsix", nonp),
+            (
+                "v1.6.8-dev.2110042330",
+                "codelldb-x86_64-darwin.vsix",
+                is_darwin_x86_64,
+            ),
+            (
+                "v1.6.8-dev.2110042330",
+                "codelldb-x86_64-linux.vsix",
+                is_linux_x86_64,
+            ),
+            (
+                "v1.6.8-dev.2110042330",
+                "codelldb-x86_64-windows.vsix",
+                nonp,
+            ),
+            ("v1.9.1", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.9.1", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.9.1", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.9.1", "codelldb-bootstrap.vsix", nonp),
+            ("v1.9.1", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.9.1", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.9.1", "codelldb-x86_64-windows.vsix", nonp),
+            ("v1.6.6", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.6.6", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.6.6", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.6.6", "codelldb-bootstrap.vsix", nonp),
+            ("v1.6.6", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.6.6", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.6.6", "codelldb-x86_64-windows.vsix", nonp),
+            ("v1.8.0", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.8.0", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.8.0", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.8.0", "codelldb-bootstrap.vsix", nonp),
+            ("v1.8.0", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.8.0", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.8.0", "codelldb-x86_64-windows.vsix", nonp),
+            ("v1.9.2", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.9.2", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.9.2", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.9.2", "codelldb-bootstrap.vsix", nonp),
+            ("v1.9.2", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.9.2", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.9.2", "codelldb-x86_64-windows.vsix", nonp),
+            ("v1.7.4", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.7.4", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.7.4", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.7.4", "codelldb-bootstrap.vsix", nonp),
+            ("v1.7.4", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.7.4", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.7.4", "codelldb-x86_64-windows.vsix", nonp),
+            ("v1.6.8", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.6.8", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.6.8", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.6.8", "codelldb-bootstrap.vsix", nonp),
+            ("v1.6.8", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.6.8", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.6.8", "codelldb-x86_64-windows.vsix", nonp),
+            (
+                "v1.9.0-dev.2302150256",
+                "codelldb-aarch64-darwin.vsix",
+                is_darwin_aarch64,
+            ),
+            (
+                "v1.9.0-dev.2302150256",
+                "codelldb-aarch64-linux.vsix",
+                is_linux_aarch64,
+            ),
+            (
+                "v1.9.0-dev.2302150256",
+                "codelldb-arm-linux.vsix",
+                is_linux_aarch64,
+            ),
+            ("v1.9.0-dev.2302150256", "codelldb-bootstrap.vsix", nonp),
+            (
+                "v1.9.0-dev.2302150256",
+                "codelldb-x86_64-darwin.vsix",
+                is_darwin_x86_64,
+            ),
+            (
+                "v1.9.0-dev.2302150256",
+                "codelldb-x86_64-linux.vsix",
+                is_linux_x86_64,
+            ),
+            (
+                "v1.9.0-dev.2302150256",
+                "codelldb-x86_64-windows.vsix",
+                nonp,
+            ),
+            (
+                "v1.6.7-dev.2109102249",
+                "codelldb-aarch64-darwin.vsix",
+                is_darwin_aarch64,
+            ),
+            (
+                "v1.6.7-dev.2109102249",
+                "codelldb-aarch64-linux.vsix",
+                is_linux_aarch64,
+            ),
+            (
+                "v1.6.7-dev.2109102249",
+                "codelldb-arm-linux.vsix",
+                is_linux_aarch64,
+            ),
+            ("v1.6.7-dev.2109102249", "codelldb-bootstrap.vsix", nonp),
+            (
+                "v1.6.7-dev.2109102249",
+                "codelldb-x86_64-darwin.vsix",
+                is_darwin_x86_64,
+            ),
+            (
+                "v1.6.7-dev.2109102249",
+                "codelldb-x86_64-linux.vsix",
+                is_linux_x86_64,
+            ),
+            (
+                "v1.6.7-dev.2109102249",
+                "codelldb-x86_64-windows.vsix",
+                nonp,
+            ),
+            ("v1.6.9", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.6.9", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.6.9", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.6.9", "codelldb-bootstrap.vsix", nonp),
+            ("v1.6.9", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.6.9", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.6.9", "codelldb-x86_64-windows.vsix", nonp),
+            (
+                "v1.6.8-dev.2110040647",
+                "codelldb-aarch64-darwin.vsix",
+                is_darwin_aarch64,
+            ),
+            (
+                "v1.6.8-dev.2110040647",
+                "codelldb-aarch64-linux.vsix",
+                is_linux_aarch64,
+            ),
+            (
+                "v1.6.8-dev.2110040647",
+                "codelldb-arm-linux.vsix",
+                is_linux_aarch64,
+            ),
+            ("v1.6.8-dev.2110040647", "codelldb-bootstrap.vsix", nonp),
+            (
+                "v1.6.8-dev.2110040647",
+                "codelldb-x86_64-darwin.vsix",
+                is_darwin_x86_64,
+            ),
+            (
+                "v1.6.8-dev.2110040647",
+                "codelldb-x86_64-linux.vsix",
+                is_linux_x86_64,
+            ),
+            (
+                "v1.6.8-dev.2110040647",
+                "codelldb-x86_64-windows.vsix",
+                nonp,
+            ),
+            ("v1.11.4", "codelldb-bootstrap.vsix", nonp),
+            ("v1.11.4", "codelldb-darwin-arm64.vsix", is_darwin_aarch64),
+            ("v1.11.4", "codelldb-darwin-x64.vsix", is_darwin_x86_64),
+            ("v1.11.4", "codelldb-linux-arm64.vsix", is_linux_aarch64),
+            ("v1.11.4", "codelldb-linux-armhf.vsix", is_linux_aarch64),
+            ("v1.11.4", "codelldb-linux-x64.vsix", is_linux_x86_64),
+            ("v1.11.4", "codelldb-win32-x64.vsix", nonp),
+            ("v1.11.2", "codelldb-bootstrap.vsix", nonp),
+            ("v1.11.2", "codelldb-darwin-arm64.vsix", is_darwin_aarch64),
+            ("v1.11.2", "codelldb-darwin-x64.vsix", is_darwin_x86_64),
+            ("v1.11.2", "codelldb-linux-arm64.vsix", is_linux_aarch64),
+            ("v1.11.2", "codelldb-linux-armhf.vsix", is_linux_aarch64),
+            ("v1.11.2", "codelldb-linux-x64.vsix", is_linux_x86_64),
+            ("v1.11.2", "codelldb-win32-x64.vsix", nonp),
+            (
+                "v1.7.0-dev.2203050810",
+                "codelldb-aarch64-darwin.vsix",
+                is_darwin_aarch64,
+            ),
+            (
+                "v1.7.0-dev.2203050810",
+                "codelldb-aarch64-linux.vsix",
+                is_linux_aarch64,
+            ),
+            (
+                "v1.7.0-dev.2203050810",
+                "codelldb-arm-linux.vsix",
+                is_linux_aarch64,
+            ),
+            ("v1.7.0-dev.2203050810", "codelldb-bootstrap.vsix", nonp),
+            (
+                "v1.7.0-dev.2203050810",
+                "codelldb-x86_64-darwin.vsix",
+                is_darwin_x86_64,
+            ),
+            (
+                "v1.7.0-dev.2203050810",
+                "codelldb-x86_64-linux.vsix",
+                is_linux_x86_64,
+            ),
+            (
+                "v1.7.0-dev.2203050810",
+                "codelldb-x86_64-windows.vsix",
+                nonp,
+            ),
+            ("v1.11.1", "codelldb-bootstrap.vsix", nonp),
+            ("v1.11.1", "codelldb-darwin-arm64.vsix", is_darwin_aarch64),
+            ("v1.11.1", "codelldb-darwin-x64.vsix", is_darwin_x86_64),
+            ("v1.11.1", "codelldb-linux-arm64.vsix", is_linux_aarch64),
+            ("v1.11.1", "codelldb-linux-armhf.vsix", is_linux_aarch64),
+            ("v1.11.1", "codelldb-linux-x64.vsix", is_linux_x86_64),
+            ("v1.11.1", "codelldb-win32-x64.vsix", nonp),
+            ("v1.7.1", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.7.1", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.7.1", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.7.1", "codelldb-bootstrap.vsix", nonp),
+            ("v1.7.1", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.7.1", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.7.1", "codelldb-x86_64-windows.vsix", nonp),
+            (
+                "v1.7.2-dev.2207161821",
+                "codelldb-aarch64-darwin.vsix",
+                is_darwin_aarch64,
+            ),
+            (
+                "v1.7.2-dev.2207161821",
+                "codelldb-aarch64-linux.vsix",
+                is_linux_aarch64,
+            ),
+            (
+                "v1.7.2-dev.2207161821",
+                "codelldb-arm-linux.vsix",
+                is_linux_aarch64,
+            ),
+            ("v1.7.2-dev.2207161821", "codelldb-bootstrap.vsix", nonp),
+            (
+                "v1.7.2-dev.2207161821",
+                "codelldb-x86_64-darwin.vsix",
+                is_darwin_x86_64,
+            ),
+            (
+                "v1.7.2-dev.2207161821",
+                "codelldb-x86_64-linux.vsix",
+                is_linux_x86_64,
+            ),
+            (
+                "v1.7.2-dev.2207161821",
+                "codelldb-x86_64-windows.vsix",
+                nonp,
+            ),
+            (
+                "v1.6.7-dev.2109180641",
+                "codelldb-aarch64-darwin.vsix",
+                is_darwin_aarch64,
+            ),
+            (
+                "v1.6.7-dev.2109180641",
+                "codelldb-aarch64-linux.vsix",
+                is_linux_aarch64,
+            ),
+            (
+                "v1.6.7-dev.2109180641",
+                "codelldb-arm-linux.vsix",
+                is_linux_aarch64,
+            ),
+            ("v1.6.7-dev.2109180641", "codelldb-bootstrap.vsix", nonp),
+            (
+                "v1.6.7-dev.2109180641",
+                "codelldb-x86_64-darwin.vsix",
+                is_darwin_x86_64,
+            ),
+            (
+                "v1.6.7-dev.2109180641",
+                "codelldb-x86_64-linux.vsix",
+                is_linux_x86_64,
+            ),
+            (
+                "v1.6.7-dev.2109180641",
+                "codelldb-x86_64-windows.vsix",
+                nonp,
+            ),
+            ("v1.6.10", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.6.10", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.6.10", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.6.10", "codelldb-bootstrap.vsix", nonp),
+            ("v1.6.10", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.6.10", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.6.10", "codelldb-x86_64-windows.vsix", nonp),
+            ("v1.6.7", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.6.7", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.6.7", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.6.7", "codelldb-bootstrap.vsix", nonp),
+            ("v1.6.7", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.6.7", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.6.7", "codelldb-x86_64-windows.vsix", nonp),
+            ("v1.7.0", "codelldb-aarch64-darwin.vsix", is_darwin_aarch64),
+            ("v1.7.0", "codelldb-aarch64-linux.vsix", is_linux_aarch64),
+            ("v1.7.0", "codelldb-arm-linux.vsix", is_linux_aarch64),
+            ("v1.7.0", "codelldb-bootstrap.vsix", nonp),
+            ("v1.7.0", "codelldb-x86_64-darwin.vsix", is_darwin_x86_64),
+            ("v1.7.0", "codelldb-x86_64-linux.vsix", is_linux_x86_64),
+            ("v1.7.0", "codelldb-x86_64-windows.vsix", nonp),
+        ];
+
+        for (version, filename, predicate) in cases {
+            assert!(predicate(filename), "{version} - {filename} assert failed");
+        }
+    }
 }
