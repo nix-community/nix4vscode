f1c806ee938f72a9d9d52471c5c206ef0ba6d02e
loongtao.zhang <loongtao.zhang@outlook.com>
2025-07-03 17:39:43 +0800

    feat(example): add more example
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- example/home.nix
+++ example/home.nix
@@ -3,6 +3,20 @@
   ...
 }:
 
+let
+  inherit (pkgs.nix4vscode)
+    forVscode
+    forVscodeVersion
+    forVscodePrerelease
+    forVscodeVersionPrerelease
+
+    forOpenVsx
+    forOpenVsxVersion
+    forOpenVsxPrerelease
+    forOpenVsxVersionPrerelease
+    ;
+in
+
 {
   home.username = "root";
   home.homeDirectory = "/root";
@@ -15,14 +29,35 @@
         default = {
           enableExtensionUpdateCheck = false;
           enableUpdateCheck = false;
-          extensions = pkgs.nix4vscode.forVscodeVersion "1.100.2" [
-            "vadimcn.vscode-lldb"
-            "ms-vscode.cpptools"
-            "jnoortheen.nix-ide"
-            # theme
-            "ms-ceintl.vscode-language-pack-zh-hans"
-            "zhuangtongfa.material-theme"
-          ];
+          extensions =
+            forVscode [
+              "zhuangtongfa.material-theme"
+            ]
+            ++ forVscodeVersion "1.100.2" [
+              "vadimcn.vscode-lldb"
+              "ms-vscode.cpptools"
+              "jnoortheen.nix-ide"
+              # theme
+              "ms-ceintl.vscode-language-pack-zh-hans"
+            ]
+            ++ forVscodePrerelease [
+              "ms-toolsai.vscode-jupyter-slideshow"
+            ]
+            ++ forVscodeVersionPrerelease "1.100.2" [
+              "ms-toolsai.vscode-jupyter-cell-tags"
+            ]
+            ++ forOpenVsx [
+              "redhat.java"
+            ]
+            ++ forOpenVsxVersion "1.100.2" [
+              "llvm-vs-code-extensions.vscode-clangd"
+            ]
+            ++ forOpenVsxPrerelease [
+              "Dart-Code.flutter"
+            ]
+            ++ forOpenVsxVersionPrerelease "1.100.2" [
+              "Continue.continue"
+            ];
         };
       };
     };
